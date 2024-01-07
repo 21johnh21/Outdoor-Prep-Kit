56d179bc21ad95eba332c80cc7cadaff51a7b32f
21johnh21 <121459080+21johnh21@users.noreply.github.com>
2024-01-07 14:51:08 -0500

    Save default trip to core data.

--- Outdoor Prep Kit/TripDefCard.swift
+++ Outdoor Prep Kit/TripDefCard.swift
@@ -10,10 +10,31 @@ import SwiftUI
 struct TripDefCard: View {
     @Binding var trip : TripDef
     
+    @Environment(\.managedObjectContext) private var viewContext
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text(trip.name)
             Text(trip.description)
+        }
+        .onTapGesture {
+            SaveTrip()
+        }
+    }
+    
+    private func SaveTrip(){
+        let newTrip = Trip(context: viewContext)
+        newTrip.name = trip.name
+        newTrip.descriptionText = trip.description
+        newTrip.timestamp = Date()
+        saveContext()
+    }
+    private func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            let nsError = error as NSError
+            fatalError("Failed to save Core Data changes: \(nsError)")
         }
     }
 }
