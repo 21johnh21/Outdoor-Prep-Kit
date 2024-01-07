9559df82c8023917ac64e2136ce2f08102c20699
21johnh21 <121459080+21johnh21@users.noreply.github.com>
2024-01-07 15:13:27 -0500

    Save items with copied trip.

--- Outdoor Prep Kit/TripDefCard.swift
+++ Outdoor Prep Kit/TripDefCard.swift
@@ -28,6 +28,19 @@ struct TripDefCard: View {
         newTrip.descriptionText = trip.description
         newTrip.timestamp = Date()
         saveContext()
+        
+        let itemManager = Items(tripID: trip.id)
+        for item in itemManager.items {
+            let newItem = Item(context: viewContext)
+            newItem.trip = newTrip
+            newItem.name = item.name
+            newItem.brand = item.brand
+            newItem.model = item.model
+            newItem.weight = item.weight
+            newItem.qty = Int16(item.qty)
+            newItem.timestamp = Date()
+            saveContext()
+        }
     }
     private func saveContext() {
         do {
