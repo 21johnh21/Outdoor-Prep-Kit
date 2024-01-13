a5a7aa5c6e20c24847addeea7934a9ef304dfef8
21johnh21 <121459080+21johnh21@users.noreply.github.com>
2024-01-12 19:52:10 -0500

    Add web area to item detail.

---
+++ Outdoor Prep Kit/WebViewContainer.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewContainer.swift
+//  Outdoor Prep Kit
+//
+//  Created by John Hawley on 1/12/24.
+//
+
+import SwiftUI
+
+struct WebViewContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebViewContainer()
+}
